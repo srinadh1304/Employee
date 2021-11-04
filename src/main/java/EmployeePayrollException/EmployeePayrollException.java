d786fc6338dad916642a68ca1f3e5152bb7d1bfb
srinadh1304 <srinadh1304@gmail.com>
2021-11-04 10:38:02 +0530

    ADDED: Custom exception class

---
+++ src/main/java/EmployeePayrollException/EmployeePayrollException.java
@@ -0,0 +1,9 @@
+package EmployeePayrollException;
+
+public class EmployeePayrollException extends RuntimeException
+{
+    public EmployeePayrollException(String message)
+    {
+        super(message);
+    }
+}
---
+++ src/main/java/com/bridgelabz/employee/exception/EmployeePayrollException.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employee.exception;
+
+public class EmployeePayrollException extends RuntimeException
+{
+    public EmployeePayrollException(String message)
+    {
+        super(message);
+    }
+}
