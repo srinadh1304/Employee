5bd9efe3b4723cf14c9648824df2da66852c626b
srinadh1304 <srinadh1304@gmail.com>
2021-11-04 10:20:23 +0530

    MODIFIED: logging info of environment property

--- src/main/java/com/bridgelabz/employee/EmployeeApplication.java
+++ src/main/java/com/bridgelabz/employee/EmployeeApplication.java
@@ -2,6 +2,8 @@ package com.bridgelabz.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
@@ -9,8 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeApplication.class, args);
-		log.info("Employee payroll app started");
+		ApplicationContext context= SpringApplication.run(EmployeeApplication.class, args);
+		log.info("Employee payroll app started in {} Environment",
+				 context.getEnvironment().getProperty("environment"));
 	}
 
 }
