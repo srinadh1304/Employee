eb831706f67f793a2fed8a69f7b0aae4de4025b8
srinadh1304 <srinadh1304@gmail.com>
2021-11-08 19:59:38 +0530

    ADDED: query to ger employee by department

--- src/main/java/com/bridgelabz/employee/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employee/repository/EmployeePayrollRepository.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.employee.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bridgelabz.employee.model.Employee;
 
 
 public interface EmployeePayrollRepository extends JpaRepository<Employee,Integer>
 {
-	
+	 @Query(value = "select * from employee_payroll ,employee_department where employee_id=id and department=:department",nativeQuery = true)
+	    List<Employee> findEmployeeDepartment(String department);
 
 }
